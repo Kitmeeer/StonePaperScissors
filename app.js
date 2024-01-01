8173cfe888107d353a7b03f6eee52f95ae1564b2
Kitmeeer <147228688+Kitmeeer@users.noreply.github.com>
2024-01-01 15:50:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+
+  // Check the condition after each game
+  if (userScore === 10 || compScore === 10) {
+    msg.innerText = "You reached the maximum limit. Please try again later.";
+    choices.forEach((choice) => {
+      choice.removeEventListener("mouseenter", handleMouseEnter);
+    });
+  }
+};
+
+const playGame = (userChoice) => {
+  // Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    // Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      // scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      // rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      // rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+// Event listener function for mouseenter
+const handleMouseEnter = (event) => {
+  const userChoice = event.target.getAttribute("id");
+  playGame(userChoice);
+};
+
+// Add event listener to each choice
+choices.forEach((choice) => {
+  choice.addEventListener("mouseenter", handleMouseEnter);
+});
